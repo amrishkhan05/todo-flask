1ae74e471e2ae2a489c74a22f29afe5a83b2af14
andydev <akanshul97@gmail.com>
2017-11-10 20:50:47 -0500

    Added database integration

--- app.py
+++ app.py
@@ -5,18 +5,33 @@ Author:  Anshul Kharbanda
 Created: 11 - 10 - 2017
 """
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
-# Create app
-app = Flask(__name__)
+# Create and configure app
+app = Flask(__name__, template_folder='views')
+app.config.from_pyfile('config.py')
+
+# Create mongo
+mongo = PyMongo(app)
+
+def get_all_todos():
+    """
+    Returns the todos from the database
+
+    :return: the todos from the database
+    """
+    return mongo.db.todos.find()
 
 # Routes
 @app.route('/')
-@app.route('/index.html')
 def index():
     """
     Index page of the app
     """
-    return render_template('index.html')
+    return render_template(
+        template_name_or_list='index.html',
+        todos=get_all_todos())
 
 # Start app
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
